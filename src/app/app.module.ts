abfeee2685a90c8d33a4b5e36395c8ba31141391
Jhoan Sebastian Duque Zapata <JDUQU13@bancodebogota.com.co>
2023-08-27 22:02:41 -0500

    Cambios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AboutMeComponent } from './components/content/about-me/about-me.component';
 import { ExperiencesComponent } from './components/content/experiences/experiences.component';
 import { IntroductionComponent } from './components/content/introduction/introduction.component';
-import { ProjectsComponent } from './components/content/projects/projects.component';
 import { StudyComponent } from './components/content/study/study.component';
-import { FooterComponent } from './components/shared/footer/footer.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
-import { SocialMediaComponent } from './components/shared/social-media/social-media.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { LayoutModule } from '@angular/cdk/layout';
@@ -21,11 +18,8 @@ import { LayoutModule } from '@angular/cdk/layout';
     AboutMeComponent,
     ExperiencesComponent,
     IntroductionComponent,
-    ProjectsComponent,
     StudyComponent,
-    FooterComponent,
     NavbarComponent,
-    SocialMediaComponent,
   ],
   imports: [
     BrowserModule,
